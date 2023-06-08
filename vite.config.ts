2bbb7b44a20f59f2c0c8a13de8b4d8a39d03d177
Cyan-Xmw <843348394@qq.com>
2023-06-08 16:51:31 +0800

    feat: 删除文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '~antd',
+        replacement: path.resolve(__dirname, 'node_modules/antd'),
+      },
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
+      },
+    ],
+  },
 })
