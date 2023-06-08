ee360277676d4a18d761c91baba4d0b6e3817b47
Cyan-Xmw <843348394@qq.com>
2023-06-08 16:50:09 +0800

    feat: 删除文件

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import XmwPer from "./components/XmwPer.vue";
-import Xmwtable from 'vue3-xmw-table'
-
-createApp(App).use(router).use(ElementPlus).use(Xmwtable).component('XmwPer', XmwPer).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: '0.0.0.0', //解决"vite use --host to expose"
-    port: 8080,
-    open: true
-  },
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: resolve(__dirname, 'src')
-      }
-    ]
-  },
-  optimizeDeps: {
-    include: ["element-plus/lib/locale/lang/zh-cn"]
-  }
+  plugins: [react()],
 })
